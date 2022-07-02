a7dfd666d6a452c64f13caad08e57b6c948e68bd
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-07-02 20:34:48 +0300

    Completed UIControls.

--- 02_UIControls/02_UIControls/ViewController.swift
+++ 02_UIControls/02_UIControls/ViewController.swift
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Push", for: .normal)
+        button.setTitle("Pushed", for: .highlighted)
+        button.frame = CGRect(x: 20, y: 20, width: 200 , height: 100)
+        button.addTarget(self, action: #selector(doSomething), for: .touchUpInside)
+        
+        view.addSubview(button)
+    }
+    
+    @objc  func doSomething(_ sender: UIButton)
+    {
+        print("Pushed")
     }
 
-
+    @IBAction func touchUpButton(_ sender: UIButton) {
+        print("hello")
+    }
+    
+    @IBAction func touchDown(_ sender: UIButton) {
+        
+        print("touch down")
+    }
+    @IBAction func sliderListener(_ slider: UISlider) {
+        print("value changed to \(slider.value)")
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        print("position: \(sender.isOn)")
+    }
 }
 
